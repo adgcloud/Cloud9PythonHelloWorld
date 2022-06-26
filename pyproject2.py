7f59010d8dc435993a7eec312bfd794c89da1cc0
adgcloud <adgcloud@gmail.com>
2022-06-26 18:15:19 +0000

    Added the advanced features of week 2 python project

--- pyproject2.py
+++ pyproject2.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3.7
 from random import *
 
-instanceprompt = int(input("How many EC2 instances do you want? : "))
-deptprompt = input("What is the name of your department? : ")
-print("Number of instances:", instanceprompt)
+
+deptchars = input("What is the name of your department? : ")
+deptprompt = deptchars.lower()
+print(deptprompt)
+if deptprompt not in ["accounting", "finops","marketing"]:
+    print("You are in the " + deptprompt + " department. Only members of FinOps, Marketing or Accounting should enter this information")
+    raise SystemExit
+else:
+    instanceprompt = int(input("How many EC2 instances do you want? : "))
+    print("Number of instances:", instanceprompt)
 for i in range(instanceprompt):
     char = random()
-    
     print("Unique instance name:", deptprompt + str(char))
