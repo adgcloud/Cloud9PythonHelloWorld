25a256d023c208c8366f139d1c3e654ce2c0ade7
adgcloud <adgcloud@gmail.com>
2022-07-05 20:45:51 +0000

    stop all instances except for cloud9

--- projectbotopython.py
+++ projectbotopython.py
@@ -4,4 +4,8 @@ import boto3
 ec2 = boto3.resource("ec2")
 
 for instance in ec2.instances.all():
-   ec2.Instance(instance.id).stop()
+    if instance.id == "i-0c870707a894bbc1a":
+        print("cloud9instance", ec2.Instance(instance.id),instance.id)
+    else:
+        print("not cloud9 instance", ec2.Instance(instance.id),instance.id)
+        #ec2.Instance(instance.id).stop()
